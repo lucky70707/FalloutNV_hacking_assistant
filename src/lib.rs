b94f521c46b2b4bce6a6fce03cd229cba4b33828
lucky70707 <luke.dekker@hva.nl>
2025-03-20 15:13:16 +0100

    added core functionality and restructured project

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,135 @@
-use std::{error::Error, fs::{self, File}, io::Write};
+use std::{
+    collections::hash_set,
+    error::Error,
+    fs::{self, File},
+    io::Write,
+};
 
-fn run(){
-    let contents = open_saved_words_file().unwrap();
-    let length: usize = 7;
-   // let mut words: Vec<String> = get_words_by_length(length, contents).unwrap();
+use std::{collections::HashSet, io};
+
+use serde_json::json;
+
+pub fn run() {
+    let unparsed_list = open_saved_words_file().unwrap();
+    let instruction: String = "Write down all the words and write 'f' when finished".to_string();
+    println!("{instruction}");
+
+    let input = process_input();
+    let length: usize = input.len();
+
+    let words: HashSet<String> = validate_words_input(input, length);
+
+    println!("The words included are: ");
+    for word in &words {
+        println!("{word}");
+    }
+
+    let output = serialize_to_json(words.clone(), length);
+    write_to_file(output);
+    println!("Pick a word and write the word here");
+    let current_word = process_input();
+    println!("How many letters were correct?");
+    let amount_correct = process_input().parse::<i8>().unwrap();
+
+    let mut remaining_words = check_word_against_list(words, current_word, amount_correct);
+
+    for word in &remaining_words {
+        println!("{word}")
+    }
+
+    
 }
 
+fn check_word_against_list(
+    words: HashSet<String>,
+    current_word: String,
+    amount_correct: i8,
+) -> Vec<String> {
+    let mut remaining_words_list: Vec<String> = Vec::new();
 
+    for word in words {
+        let mut overlap_count: i8 = 0;
+        for i in 0..word.len() {
+            if word.chars().nth(i) == current_word.chars().nth(i) {
+                overlap_count += 1;
+            }
+        }
+        if overlap_count == amount_correct {
+            remaining_words_list.push(word);
+        }
+    }
+    remaining_words_list
+}
 
-
-
-fn overwrite_words_json(){
-    
+fn overwrite_words_json() {
     let filename = "saved_words2.json".to_string();
     let mut file = File::create(filename);
-    
-
 }
 
+//needs to deal with errors inside function, file not found error should be handled by creating the file
 fn open_saved_words_file() -> Result<String, Box<dyn Error>> {
     let file = "saved_words.json".to_string();
     let contents = fs::read_to_string(file)?;
     Ok(contents)
 }
+
+fn serialize_to_json(words: HashSet<String>, length: usize) -> String {
+    let words: Vec<String> = words.into_iter().collect();
+    let test = serde_json::Value::from(words);
+    let json_text = json!({
+        format!("length{length}"):
+            test
+    });
+
+    //println!("{json_text}");
+    //println!("test: {test}");
+
+    json_text.to_string()
+}
+
+fn write_to_file(output: String) -> std::io::Result<()> {
+    let mut file = File::create("test.txt")?;
+    let output = output;
+    println!("{output}");
+    file.write_all(output.as_bytes())?;
+    Ok(())
+}
+
+fn validate_words_input(input: String, length: usize) -> HashSet<String> {
+    let mut input = input;
+    let mut words: HashSet<_> = HashSet::new();
+    // let mut log:String;
+    //let already_exists_error= "word {} has already been inserted before.".to_string();
+
+    while input.ne("f") {
+        if validate_single_word(&input, length) {
+            words.insert(input);
+            //  log.push(format!(already_exists_error,input));
+        }
+        input = process_input();
+    }
+
+    words
+}
+
+fn validate_single_word(input: &String, length: usize) -> bool {
+    if input.len() == length {
+        true
+    } else {
+        println!("latest word is a different length than previous word(s)");
+        false
+    }
+}
+
+fn process_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    //the terminal will add characters even if you have only typed a single character, so we trim the input here
+    input.trim().to_string()
+}
+
 /*
 fn get_words_by_length(length: usize, contents: String) -> Result<Vec<String>, String> {
     let mut parsed = serde_json::Deserializer::from_str(&contents);
@@ -31,13 +138,12 @@ fn get_words_by_length(length: usize, contents: String) -> Result<Vec<String>, S
 
 
     Ok(array)
-    
+
 }*/
 
 #[cfg(test)]
 mod tests {
-    use crate::{ open_saved_words_file};
-
+    use super::*;
 
     #[test]
     fn test_open_saved_words_file() {
@@ -46,10 +152,27 @@ mod tests {
     }
 
     #[test]
+    fn input_validation_test() {
+        let input = "healing".to_string();
+        let length = input.len();
+    }
+
+    #[test]
     fn test_get_words_by_length() {
         let contents = open_saved_words_file().unwrap();
         let result = get_words_by_length(7 as usize, contents).unwrap();
         let expected_result = vec!["help".to_string()];
         assert_eq!(result, expected_result);
+    }
+
+    #[test]
+
+    fn validate_json_serialization() {
+        let mut words: HashSet<String> = HashSet::new();
+        words.insert("ceiling".to_string());
+        words.insert("special".to_string());
+        words.insert("looking".to_string());
+        let length: usize = "ceiling".len();
+        serialize_to_json(words, length);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-
-use std::io::Write;
-use std::{collections::HashSet, fs::File, io};
-
-use serde_json::json;
 //TODO
 //function that gets all values from saved_words.json or creates saved_words.json if it doesn't exist
 //the values from the array that equal the length of user words being added are to be converted into a hashset
@@ -13,111 +8,26 @@ use serde_json::json;
 //add GUI functionality using slint
 
 //NOTE
-//can test out efficacy of program with https://jetholt.com/hacking/
+//can test out efficacy of program with https://jetholt.com/hacking/ and https://pfndesign.itch.io/termlink-access
+
+//the words in the minigame have lengths ranging from 4 to 15 according to https://fallout.wiki/wiki/Hacking_(Fallout:_New_Vegas)
+/* 
+struct WordsWithLengths{
+    length4: Vec<String>,
+    length5: Vec<String>,
+    length6: Vec<String>,
+    length7: Vec<String>,
+    length8: Vec<String>,
+    length9: Vec<String>,
+    length10: Vec<String>,
+    length11: Vec<String>,
+    length12: Vec<String>,
+    length13: Vec<String>,
+    length14: Vec<String>,
+    length15: Vec<String>,
+}*/
+
+
 fn main() {
-    let instruction: String = "Write down all the words and write 'f' when finished".to_string();
-    println!("{instruction}");
-
-    let input = process_input();
-    let length: usize = input.len();
-
-    let words: HashSet<String> = validate_words_input(input, length);
-
-    println!("The words included are: ");
-    for word in &words {
-        println!("{word}");
+        fallout_hacking_assistant::run();   
     }
-
-    let output = serialize_to_json(words, length);
-    let result = write_to_file(output);
-    result.unwrap_err()
-    {
-        println!("Writing to file failed");
-    }
-}
-#[test]
-
-    fn validate_json_serialization(){
-        let mut words: HashSet<String> = HashSet::new();
-        words.insert("ceiling".to_string());
-        words.insert("special".to_string());
-        words.insert("looking".to_string());
-        let length : usize = "ceiling".len();
-        serialize_to_json(words, length);
-    }
-
-
-fn serialize_to_json(words: HashSet<String>, length: usize)->String{
-    let words: Vec<String> = words.into_iter().collect();
-    let test = serde_json::Value::from(words);
-    let json_text = json!({
-        format!("length{length}"):
-            test      
-    });
-   
-    //println!("{json_text}");
-    //println!("test: {test}");
-    
-    json_text.to_string()
-}
-
-fn write_to_file(output: String) -> std::io::Result<()>{
-    let mut file = File::create("test.txt")?;
-    let output=output;   
-    println!("{output}");
-    file.write_all(output.as_bytes())?;
-    Ok(())
-}
-
-fn validate_words_input(input: String, length: usize) -> HashSet<String> {
-    let mut input = input;
-    let mut words: HashSet<_> = HashSet::new();
-   // let mut log:String;
-    //let already_exists_error= "word {} has already been inserted before.".to_string();
-
-    while input.ne("f") {
-        if validate_single_word(&input, length) {
-            words.insert(input) ;
-              //  log.push(format!(already_exists_error,input));
-            
-        }
-        input = process_input();
-    }
-
-
-
-    words
-}
-
-fn validate_single_word(input: &String, length: usize) -> bool {
-    if input.len() == length {
-        true
-    } else {
-        println!("latest word is a different length than previous word(s)");
-        false
-    }
-}
-
-fn process_input() -> String {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    //the terminal will add characters even if you have only typed a single character, so we trim the input here
-    input.trim().to_string()
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashSet;
-
-    use crate::serialize_to_json;
-
-    #[test]
-    fn input_validation_test() {
-        let input = "healing".to_string();
-        let length = input.len();
-    }
-
-    
-}
